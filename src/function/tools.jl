33b37e514bff333cb17e18277b0d0779e2f272ee
Yuya Nagano <yuya@YuyanoMac-mini.local>
2024-11-08 07:26:31 +0900

    test_commit

--- src/function/tools.jl
+++ src/function/tools.jl
@@ -1,42 +1,42 @@
-function unique_theta_num(num, NSIDE)
-    # This function calculates the unique theta number range for a given pixel number and NSIDE parameter.
+function unique_theta_num(num, nside)
+    # This function calculates the unique theta number range for a given pixel number and nside parameter.
     #
     # # Arguments
     # - `num::Int`: The pixel number for which the unique theta number range is to be calculated.
-    # - `NSIDE::Int`: The NSIDE parameter, which determines the resolution of the HEALPix grid.
+    # - `nside::Int`: The nside parameter, which determines the resolution of the HEALPix grid.
     #
     # # Returns
     # - `Tuple{Int, Int}`: A tuple containing the start and stop indices of the unique theta number range.
     #
-    # The function first calculates the total number of pixels (NPIX) for the given NSIDE using the `nside2npix` function.
-    # It then determines the start and stop indices based on the value of `num` relative to `NSIDE` and `3NSIDE`.
+    # The function first calculates the total number of pixels (npix) for the given nside using the `nside2npix` function.
+    # It then determines the start and stop indices based on the value of `num` relative to `nside` and `3nside`.
     # The calculations are divided into three cases:
-    # 1. When `num` is less than `NSIDE + 1`.
-    # 2. When `num` is less than `3NSIDE + 1`.
-    # 3. When `num` is greater than or equal to `3NSIDE + 1`.
+    # 1. When `num` is less than `nside + 1`.
+    # 2. When `num` is less than `3nside + 1`.
+    # 3. When `num` is greater than or equal to `3nside + 1`.
     #
     # The function returns the start and stop indices as integers.
-    NPIX = nside2npix(NSIDE)
+    npix = nside2npix(nside)
     n = num-1
     a1 = 4
     d = 4
-    if num < NSIDE + 1
+    if num < nside + 1
         start = 1/2*n*(2*a1+(n-1)*d)+1
         stop  = 1/2*num*(2*a1+(num-1)*d)
-    elseif num < 3NSIDE+1
-        n_2 = num - NSIDE
-        n = NSIDE - 1
-        start = 1/2*NSIDE*(2*a1+(NSIDE-1)*d) + 4*(n_2-1)*NSIDE + 1
-        stop = 1/2*NSIDE*(2*a1+(NSIDE-1)*d) + 4*(n_2)*NSIDE 
+    elseif num < 3nside+1
+        n_2 = num - nside
+        n = nside - 1
+        start = 1/2*nside*(2*a1+(nside-1)*d) + 4*(n_2-1)*nside + 1
+        stop = 1/2*nside*(2*a1+(nside-1)*d) + 4*(n_2)*nside 
     else
-        n_2 = 4NSIDE-1 - num
-        start = NPIX - 1/2*(n_2+1)*(2*a1+(n_2)*d) +1
-        stop = NPIX - 1/2*(n_2)*(2*a1+(n_2-1)*d)
+        n_2 = 4nside-1 - num
+        start = npix - 1/2*(n_2+1)*(2*a1+(n_2)*d) +1
+        stop = npix - 1/2*(n_2)*(2*a1+(n_2-1)*d)
     end
     return Int(start), Int(stop)
 end
 
-function　unique_theta_val(nside, res)
+function unique_theta_val(nside, res)
     npix = nside2npix(nside)
     θ= zeros(npix)
     for i in 1:npix
