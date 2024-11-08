475f6c05e59539040c3ec1b5ddae5d89eada2103
Yuya Nagano <psoo3ak2@s.okayama-u.ac.jp>
2024-11-08 09:40:24 +0900

    add export of func

--- src/EBC.jl
+++ src/EBC.jl
@@ -1,3 +1,8 @@
 include("./function/rotaters.jl")
 include("./function/structures.jl")
 include("./function/tools.jl")
+
+
+export simple_rotater
+export gen_ConvolutionParams
+export alm_idx, for_healpy_order, unique_theta_num, unique_theta_val
