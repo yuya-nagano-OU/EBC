8d2587bfbea06f932a409a06cdd4bc388954756c
Yuya Nagano <psoo3ak2@s.okayama-u.ac.jp>
2024-11-08 16:21:22 +0900

    delete file

--- src/.ipynb_checkpoints/EBC-checkpoint.jl
+++
@@ -1,8 +0,0 @@
-include("./function/rotaters.jl")
-include("./function/structures.jl")
-include("./function/tools.jl")
-
-
-export simple_rotater, swignerd_calc
-export gen_ConvolutionParams
-export alm_idx, for_healpy_order, unique_theta_num, unique_theta_val
