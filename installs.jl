08e7ed77fccd624b7b36867c43f2683d244134eb
Yuya Nagano <psoo3ak2@s.okayama-u.ac.jp>
2024-12-08 23:56:52 +0900

    add files

---
+++ installs.jl
@@ -0,0 +1,16 @@
+using Pkg
+
+
+Pkg.add("Plots")
+Pkg.add("PyCall")
+Pkg.add("PyPlot")
+
+Pkg.add("WignerD")
+Pkg.add("Healpix")
+
+
+Pkg.add("LinearAlgebra")
+Pkg.add("StaticArrays")
+
+Pkg.add("NPZ")
+Pkg.add("BenchmarkTools")
